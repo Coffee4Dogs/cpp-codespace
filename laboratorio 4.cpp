ed61c977c838ecca7a0bd10846126efdf5b75501
Jack H. Lagos <59121551+Coffee4Dogs@users.noreply.github.com>
2024-02-10 00:25:28 +0000

    asda

---
+++ laboratorio 4.cpp
@@ -0,0 +1,27 @@
+// #Cuenta: 32011727 -> Jack Hollmann Lagos Mejia
+#include <iostream>
+#include <queue>
+
+using std::queue;
+using std::string;
+
+int main() {
+    
+    //Declarar Queue/Cola
+        queue<string> libros; 
+
+    //Agregar Elementos a la cola o queue:
+        libros.push("El Señor de los Anillos");     
+        libros.push("Harry Potter");
+        libros.push("El Codigo da Vinci");
+        libros.push("Lo que el viento se llevo");
+        libros.push("Padre Rico Padre Pobre");
+        libros.push("La Biblia");
+
+
+
+
+
+    std::cout << "Prgrama termino con exito." << '\n';
+    return 0;
+}
